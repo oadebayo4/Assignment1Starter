f140ad1db9829c498a9d6933077b7c2e90c8ea54
Lanre Adebayo <46577127+oadebayo4@users.noreply.github.com>
2019-10-07 13:18:02 -0600

    moved the folder into the repo
    Javascript Assigment 1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Enter your JavaScript for the solution here...
+// Get refences to the select, top text and bottom text
+
+const memeImage = document.querySelector('#meme-image')
+const memeTopText = document.querySelector('#meme-top-text')
+const memeTopText = document.querySelector('#meme-bottom-text')
+
+memeImage.addEventListener('change', (e) => {
+    
+})

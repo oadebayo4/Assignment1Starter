9b50c54f379d0928e7ea57a9d3394d166d1b12eb
Lanre Adebayo <46577127+oadebayo4@users.noreply.github.com>
2019-10-09 13:55:22 -0600

    created the image and text eventlistners

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
 // Enter your JavaScript for the solution here...
 // Get refences to the select, top text and bottom text
 
-const memeImage = document.querySelector('#meme-image')
-const memeTopText = document.querySelector('#meme-top-text')
-const memeTopText = document.querySelector('#meme-bottom-text')
+const memeOptions = document.querySelector('#meme-image')
+const inputTopText = document.querySelector('#meme-top-text')
+const inputBottomText = document.querySelector('#meme-bottom-text')
+const memeImage = document.querySelector('.meme-display img')
+const memeTopText = document.querySelector('.meme-display .top-text')
+const memeBottomText = document.querySelector('.meme-display .bottom-text')
 
-memeImage.addEventListener('change', (e) => {
+
+
+
+memeOptions.addEventListener('change', (e) => {
+    const imageUrl = `/img/${event.target.value}.png`;
+    console.log(imageUrl);
+    
+    memeImage.src = imageUrl;
+    const temp = Array.from (e.target.options)
+    const index = e.target.selectedIndex
+   console.log(temp[index])
+
+})
+
+inputTopText.addEventListener('keydown', (e) =>{
+    memeTopText.innerHTML = e.target.value
+    
+})
+inputBottomText.addEventListener('keydown', (e) =>{
+    memeBottomText.innerHTML = e.target.value
     
 })
